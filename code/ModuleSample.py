b1d080db85ee09656ad99a19feb526b7fdae0d26
Ansu-John <ansujohn09@gmail.com>
2021-07-27 21:17:54 +0800

    Adding module

---
+++ code/ModuleSample.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def downloadFile(url):
+    print("Establish Connection : "+url)
+    print("Open Connection")
+    print("Download Data")
+    print("Close Connection"+'\n')
+
